1decacf83f5d3e5ca0bee0871531e44ad3377bd2
mirela2109 <mkaramfilova5@gmail.com>
2018-12-02 17:19:47 +0200

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter binary number: ");
+            string binary = Console.ReadLine();
+            Console.WriteLine("{0} to decimal is {1}.",
+                binary, Convert.ToInt64(binary, 2));
+
+        }
+    }
+}
